2596bf243039db0f6550c637692e26a9d90cf765
Kelsey Brown <kbrown55347@gmail.com>
2021-11-25 12:55:21 -0600

    created and tested axios GET /gallery route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// import components
+
 
 function App() {
-    return (
+
+  // The equivalent to $(document).ready(onReady);
+  useEffect(() => {
+  // Call fetchGallery on load (and only once, on load)
+    fetchGallery();
+  }, []);
+
+  // create function to fetch gallery
+  const fetchGallery = () => {
+    // axios GET /gallery route
+    axios({
+    method: 'GET',
+    url: '/gallery'
+    }).then((response) => {
+    console.log('in GET /gallery route', response.data);
+    }).catch((error) => {
+    console.log('GET /gallery failed', error);
+    });
+  }; // end fetchGallery
+    
+  return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* <img src="images/goat_small.jpg"/> */}
       </div>
     );
-}
+  } // end App
 
 export default App;
